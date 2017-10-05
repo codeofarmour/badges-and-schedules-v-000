7ddf3e5c031be395d5e9f2e2fd0eee5931daeb0b
Terri Riggle <codeofarmour@outlook.com>
2017-10-05 17:27:35 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -17,7 +17,7 @@ def assign_rooms(speakers)
 end
 
 def printer(speakers)
-  batch_badge_creator(speakers).each do |assignment|
+  batch_badge_creator(speakers).each do |badge|
     puts badge
   end
 
