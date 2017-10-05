8c3fe71e89162e778d72c53f2dc7a2fa3cb2005a
Terri Riggle <codeofarmour@outlook.com>
2017-10-05 17:15:15 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,5 +1,4 @@
 def badge_maker(names)
   speakers = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
-  speakers.each{|names|} 
-    print "Hello, my name is #{names}."
+  speakers.each{|names|} print "Hello, my name is #{names}."
 end
